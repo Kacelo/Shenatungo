47c5fbdcf506596cf15a7a9ae25f3905484611d2
vetu <v2ndjavera@gmail.com>
2023-06-22 16:23:34 +0200

    Searchfiltertroubleshout

--- shena-front-end/components/eNavbar/DesktopNav.tsx
+++ shena-front-end/components/eNavbar/DesktopNav.tsx
@@ -1,8 +1,9 @@
-// import { navItems } from '@/helpers';
+import { navItems } from '../../app/helpers';
 import { Box, Flex, Stack } from '@chakra-ui/react';
 import Link from 'next/link';
 import { AppLogo } from '../AppLogo';
 import { desktopNavStyles } from './style';
+import { Search } from '../Search/Search';
 // import { AppLogo } from '../AppLogo';
 // import { Cart } from '../Cart/Cart';
 // import { Wishlist } from '../Wishlist/Wishlist';
@@ -16,13 +17,14 @@ export function DesktopNav() {
           <AppLogo/>
         </Box>
 
-        {/* {navItems.map((navItem) => (
+        {navItems.map((navItem) => (
           <Box key={navItem.label}>
             <Link href={navItem.href}>{navItem.label}</Link>
           </Box>
-        ))} */}
+        ))}
 
         {/* <Search /> */}
+        {/* <Search/> */}
       </Stack>
 
       <Stack direction="row" spacing={2}>
