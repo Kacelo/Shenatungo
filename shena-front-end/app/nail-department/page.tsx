8c482cf49b1d77794a0397bf9ed8e9a3c10d0577
Vernon <vernonkachelo@gmail.com>
2023-08-02 16:53:21 +0200

    added nail department

---
+++ shena-front-end/app/nail-department/page.tsx
@@ -0,0 +1,92 @@
+"use client";
+import ServicesHero from "@/components/ServicesHero";
+import { PopularProducts } from "@/components/popularProducts";
+import Gallery from "@/components/Gallery";
+import HorizontalRule from "@/components/HorizontalRule";
+import EmployeeCards from "@/components/EmployeeCards";
+import MakeUpIntro from "@/components/make-up-intro";
+export default function MakeUpStudio() {
+  const images = [
+    ...Array.from({ length: 2 }, (_, index) => ({
+      src: `/images/nail-department/photo_5924641007121578${865 + index}_y.jpg`,
+      alt: `Image ${index + 3}`,
+      caption: `BY Lucy`,
+      price: "",
+    })),
+    ...Array.from({ length: 3 }, (_, index) => ({
+      src: `/images/nail-department/photo_5929321095545076${599 + index}_y.jpg`,
+      alt: `Image ${index + 3}`,
+      caption: ``,
+      price: "",
+    })),
+    ...Array.from({ length: 2 }, (_, index) => ({
+      src: `/images/nail-department/photo_5926892806935264${605 + index}_x.jpg`,
+      alt: `Image ${index + 3}`,
+      caption: ``,
+      price: "",
+    })),
+    ...Array.from({ length: 2 }, (_, index) => ({
+      src: `/images/nail-department/photo_5926892806935264${608 + index}_x.jpg`,
+      alt: `Image ${index + 3}`,
+      caption: ``,
+      price: "",
+    })),
+  ];
+  const employeesData = [
+    {
+      name: "Lebby",
+      phoneNumber: "+264814504952",
+      socialMedia: "https://www.instagram.com/lebbyz_fade_cutz2023/",
+      imageSrc: "/images/barberSection/lebby2.jpeg",
+      instagramHandle: "@lebbyz_fade_cutz2023",
+      whatsapp: true,
+    },
+    {
+      name: "Penda",
+      phoneNumber: "+1 987-654-3210",
+      socialMedia: "",
+      imageSrc: "/images/barberSection/penda2.jpg",
+      instagramHandle: "@lebbyz_fade_cutz2023",
+      whatsapp: false,
+    },
+    {
+      name: "Santos",
+      phoneNumber: "+264852924965",
+      socialMedia: "https://www.instagram.com/santos_di_barber/",
+      imageSrc: "/images/barberSection/santos2.jpg",
+      instagramHandle: "@santos_di_barber",
+      whatsapp: true,
+    },
+  ];
+  const introductoryMessage = [
+    {
+      headingText: "Meet Our Talented Make Up Artists",
+      meetOur: "",
+    },
+  ];
+  return (
+    <>
+      <ServicesHero
+        imageUrl="/images/nail-department/photo_5926892806935264605_x.jpg"
+        introText="NAIL DEPARTMENT"
+        headerImage=""
+      />
+      <MakeUpIntro />
+      <HorizontalRule />
+
+      <Gallery
+        images={images}
+        introText="Our Captivating Makeup Studio Gallery"
+      />
+      <HorizontalRule />
+
+      <PopularProducts />
+
+      <HorizontalRule />
+      <EmployeeCards
+        employees={employeesData}
+        openingText={introductoryMessage}
+      />
+    </>
+  );
+}
