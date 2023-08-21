dc98aadb4cc6fc5309b232696e4931fc05e9f858
vetu <v2ndjavera@gmail.com>
2023-08-21 09:34:14 +0200

    new site changes

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,7 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {},
+  "cssModules": {
+    "C:\\Users\\VetuN\\Desktop\\Shenatungo\\app\\page": [],
+    "C:\\Users\\VetuN\\Desktop\\Shenatungo\\app\\eShop\\page": []
+  }
+}
